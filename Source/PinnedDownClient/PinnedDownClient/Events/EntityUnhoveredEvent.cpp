0da305dc24a1b370369d10020cb9365291c43381
Nick Prühs <dev@npruehs.de>
2014-10-31 14:36:58 +0100

    ADDED: UI - Hovering cards by tapping and holding.

---
+++ Source/PinnedDownClient/PinnedDownClient/Events/EntityUnhoveredEvent.cpp
@@ -0,0 +1,4 @@
+#include "pch.h"
+#include "Events\EntityUnhoveredEvent.h"
+
+const HashedString PinnedDownClient::Events::EntityUnhoveredEvent::EntityUnhoveredEventType = HashedString("EntityUnhovered");
--- Source/PinnedDownClient/PinnedDownClient/Systems/UIInteractionSystem.h
+++ Source/PinnedDownClient/PinnedDownClient/Systems/UIInteractionSystem.h
@@ -7,6 +7,7 @@
 #include "Events\AudioEngineChangedEvent.h"
 #include "EntityInitializedEvent.h"
 #include "EntityRemovedEvent.h"
+#include "Events\PointerPressedEvent.h"
 #include "Events\PointerReleasedEvent.h"
 
 #include "Systems\UI\Button.h"
@@ -27,11 +28,17 @@ namespace PinnedDownClient
 			UIInteractionSystem();
 
 			void InitSystem(PinnedDownCore::Game* game);
+			void Update(float dt);
 
 		private:
 			ComPtr<IXAudio2> soundAudioEngine;
 
 			std::list<Button> buttons;
+
+			UI::Button* currentButton = nullptr;
+			float elapsedTapTime = 0.0f;
+			float hoverThreshold = 1.0f;
+			
 
 			void LoadResources();
 
@@ -40,7 +47,10 @@ namespace PinnedDownClient
 			void OnAudioEngineChanged(AudioEngineChangedEvent& audioEngineChangedEvent);
 			void OnEntityInitialized(EntityInitializedEvent& entityInitializedEvent);
 			void OnEntityRemoved(EntityRemovedEvent& entityRemovedEvent);
+			void OnPointerPressed(PointerPressedEvent& pointerPressedEvent);
 			void OnPointerReleased(PointerReleasedEvent& pointerReleasedEvent);
+			
+			UI::Button* HitTest(Vector2F position);
 		};
 	}
 }
