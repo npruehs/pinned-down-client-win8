15d6467c8e1c045246bd1563fbc7c8d289fb8f05
Nick Prühs <dev@npruehs.de>
2015-01-09 18:22:52 +0100

    CHANGED: Localization - Removed trailing slash from server status URL.

--- Source/PinnedDownClient/PinnedDownClient/Assets/Text/English.lua
+++ Source/PinnedDownClient/PinnedDownClient/Assets/Text/English.lua
@@ -47,7 +47,7 @@ Card_Type_Starship = "Starship"
 Error_AuthenticationRequired = "You must authenticate yourself in order to play Pinned Down."
 Error_NotAllEnemyShipsAssigned = "You have to assign all of your ships to fight!"
 Error_NotAllFightsResolved = "You have to resolve all fights first!"
-Error_UnableToConntect = "Unable to connect to server.\r\n\r\nCheck http://pinneddown.de/server/ if you're sure you've done everything right!"
+Error_UnableToConntect = "Unable to connect to server.\r\n\r\nCheck http://pinneddown.de/server if you're sure you've done everything right!"
 
 
 GameScreen_Button_EndTurnPhase = "End Turn Phase"
